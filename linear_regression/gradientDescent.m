b0db973f0f3171e596d341d48b8c1e6fa5506383
Allen Ning <npc1222@icloud.com>
2018-01-13 19:47:51 +1100

    added gradientDescent solution

--- linear_regression/gradientDescent.m
+++ linear_regression/gradientDescent.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,18 +15,19 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
     % ============================================================
+    total1 = 0
+    total2 = 0
+    for i = 1:m
+      total1 += (theta(1,1) * X(i,1) + theta(2,1) * X(i,2) - y(i,1)) * X(i,1)
+      total2 += (theta(1,1) * X(i,1) + theta(2,1) * X(i,2) - y(i,1)) * X(i,2)
+    end
 
-    % Save the cost J in every iteration    
+    theta(1,1) = theta(1,1) - (alpha * total1) / m
+    theta(2,1) = theta(2,1) - (alpha * total2) / m
+
+    % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
-
 end
 
 end
