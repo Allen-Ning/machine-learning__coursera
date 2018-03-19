21c2e7897fd94475e6cd20a6a70c818fac4d7938
Allen Ning <npc1222@icloud.com>
2018-03-19 19:32:57 +1100

    added logistic regression

---
+++ logistic_regression/costFunction.m
@@ -0,0 +1,47 @@
+function [J, grad] = costFunction(theta, X, y)
+  J = 0
+  for k = 1 : size(X, 1)
+    x = X(k, :)
+    J += -y(k) * log(h(theta, x)) - (1 - y(k)) * log(1 - h(theta, x))
+  end
+  J = J / size(X, 1)
+  
+  grad = zeros(length(theta), 1)
+  for k = 1 : size(theta, 1)
+    grad(k) = gradFunc(theta, X, y, k)
+  end
+  grad
+end
+
+% grad
+function result = gradFunc(theta, X, y,index)
+  result = 0
+  for k = 1 : size(X, 1)
+    x = X(k, :)
+    if index == 1
+      result += h(theta, x) - y(k)
+    else
+      result += (h(theta, x) - y(k)) * x(index)
+    end
+  end
+  result = result / size(X, 1)
+end
+
+% hypothesize
+function h = h(theta, x)
+  h = sigmoid(l(theta, x))
+end
+
+% linear regression
+function l = l(theta, x)
+  l = 0
+  for k = 1 : length(theta)
+    l +=  theta(k) * x(k)
+  end
+end
+
+% sigmoid
+%function g = g(z)
+%  g = 1 ./ ( 1 + exp(-z) )
+%end
+
