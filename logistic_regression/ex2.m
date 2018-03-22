2b07243e216e3f95f19e0fb3a83637da2e253805
Allen Ning <npc1222@icloud.com>
2018-03-22 18:31:15 +1100

    added predict

--- logistic_regression/ex2.m
+++ logistic_regression/ex2.m
@@ -59,20 +59,17 @@ pause;
 
 % Add intercept term to x and X_test
 X = [ones(m, 1) X];
-
 % Initialize fitting parameters
-%initial_theta = zeros(n + 1, 1);
+initial_theta = zeros(n + 1, 1);
 
 % Compute and display initial cost and gradient
-%[cost, grad] = costFunction(initial_theta, X, y);
+[cost, grad] = costFunction(initial_theta, X, y);
 
-%fprintf('Cost at initial theta (zeros): %f\n', cost);
-%fprintf('Expected cost (approx): 0.693\n');
-%pause
-%fprintf('Gradient at initial theta (zeros): \n');
-%fprintf(' %f \n', grad);
-%fprintf('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
-%pause
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Expected cost (approx): 0.693\n');
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+fprintf('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
 
 % Compute and display cost and gradient with non-zero theta
 test_theta = [-24; 0.2; 0.2];
@@ -80,14 +77,10 @@ test_theta = [-24; 0.2; 0.2];
 
 fprintf('\nCost at test theta: %f\n', cost);
 fprintf('Expected cost (approx): 0.218\n');
-pause
 fprintf('Gradient at test theta: \n');
 fprintf(' %f \n', grad);
 fprintf('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n');
-pause
 fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
 
 %% ============= Part 3: Optimizing using fminunc  =============
 %  In this exercise, you will use a built-in function (fminunc) to find the
@@ -98,8 +91,7 @@ options = optimset('GradObj', 'on', 'MaxIter', 400);
 
 %  Run fminunc to obtain the optimal theta
 %  This function will return theta and the cost 
-[theta, cost] = ...
-	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
 
 % Print theta to screen
 fprintf('Cost at theta found by fminunc: %f\n', cost);
@@ -123,8 +115,7 @@ legend('Admitted', 'Not admitted')
 hold off;
 
 fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
+pause
 %% ============== Part 4: Predict and Accuracies ==============
 %  After learning the parameters, you'll like to use it to predict the outcomes
 %  on unseen data. In this part, you will use the logistic regression model
