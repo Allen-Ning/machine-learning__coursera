777001ef67ffbc9e96335ca91f921325708fc4d8
Allen Ning <npc1222@icloud.com>
2018-03-25 12:56:48 +1100

    added regularize function

--- logistic_regression/costFunctionReg.m
+++ logistic_regression/costFunctionReg.m
@@ -1,27 +1,47 @@
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
-%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
-%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+  J = 0
+  m = size(X, 1)
+  for k = 1 : m
+    x = X(k, :)
+    J += -y(k) * log(h(theta, x)) - (1 - y(k)) * log(1 - h(theta, x))
+  end
+  J = J / size(X, 1) + regularize(theta, lambda, m)
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  grad = zeros(length(theta), 1)
+  for k = 1 : size(theta, 1)
+    grad(k) = gradFunc(theta, X, y, k, lambda, m)
+  end
+end
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+% grad
+function result = gradFunc(theta, X, y,index, lambda, m)
+  result = 0
+  for k = 1 : size(X, 1)
+    x = X(k, :)
+    if index == 1
+      result += h(theta, x) - y(k)
+    else
+      result += (h(theta, x) - y(k)) * x(index) + theta(index) * lambda / m
+    end
+  end
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+  result = result / size(X, 1)
+end
 
+function result = regularize(theta, lambda, m)
+  result = 0
+  for i = 2: length(theta)
+    result += theta(i) ^ 2
+  end
+  result = lambda * result / (2 * m)
+end
 
+% hypothesize
+function h = h(theta, x)
+  h = sigmoid(l(theta, x))
+end
 
-
-
-
-% =============================================================
-
+% linear regression
+function l = l(theta, x)
+  l = x * theta
 end
